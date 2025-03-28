54d0b1eb3f4ce1b937d999c06a4d4a40dccf57d3
Dobrosaf Tufegdžić <d.tufegdzic-int@levi9.com>
2025-03-28 11:46:17 +0100

    feat: chat box now scrollable

--- frontend/llm-aws-search/src/app/app.component.ts
+++ frontend/llm-aws-search/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component,ElementRef,ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AppService } from './services/app.services';
+import { ChangeDetectorRef } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,13 +10,15 @@ import { AppService } from './services/app.services';
   standalone:false,
   
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked{
   title = 'llm-aws-search';
 thinking: boolean=false;
+@ViewChild('chatBox') chatBox: ElementRef | undefined;
 userInput: string = ''; // Variable to bind input value
 result:string='';
 messages:string [] = [];  // Array to store the chat messages
-constructor(private appService: AppService) { }
+time:string=new Date().toLocaleTimeString();
+constructor(private appService: AppService,private cdRef: ChangeDetectorRef) { }
 
 
   // Function to handle form submission
@@ -25,11 +28,21 @@ constructor(private appService: AppService) { }
     const payload = { message: this.userInput };
     this.messages.push("You: "+this.userInput);
     this.thinking=true;
+    this.autoScroll();
     // Send the POST requestq
     this.appService.recieveUserInput(payload).subscribe(res=> { this.result = res; });
     console.log(this.result);
     this.messages.push("ChatBot: "+this.result);
     this.thinking=false;
     console.log(payload);
+  }
+  ngAfterViewChecked(): void {
+    this.autoScroll();
+  }
+  private autoScroll(): void {
+    const chatBoxElement = this.chatBox?.nativeElement;
+    if (chatBoxElement) {
+      chatBoxElement.scrollTop = chatBoxElement.scrollHeight;
+    }
   }
 }
