66ae3add24e6d72b9feba9f58c9a6fb8705c2a1c
Nikola <n.maric1912@gmail.com>
2025-03-28 13:50:19 +0100

    fix: refactor project structure

--- backend/backend/backend_stack.py
+++ backend/backend/backend_stack.py
@@ -28,69 +28,68 @@ class BackendStack(Stack):
                                     )
         
         
-
-        lambda_role = iam.Role(
-            self, "LambdaRole",
-            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com")
-        )
-        lambda_role.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole")
-        )
-        lambda_role.add_managed_policy(
-        iam.ManagedPolicy.from_aws_managed_policy_name("AmazonAPIGatewayInvokeFullAccess")
-        )
-        lambda_role.add_managed_policy(
-        iam.ManagedPolicy.from_aws_managed_policy_name("SecretsManagerReadWrite")
-        )
-
+        lambda_role = iam.Role(self, "LambdaRole",
+                               assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+                               managed_policies=[
+                                   iam.ManagedPolicy.from_aws_managed_policy_name("SecretsManagerReadWrite"),
+                                    iam.ManagedPolicy.from_aws_managed_policy_name("AmazonAPIGatewayInvokeFullAccess"),
+                                   iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole"),
+                                   iam.ManagedPolicy.from_aws_managed_policy_name("AmazonBedrockFullAccess")
+                               ])
+        
         jira_secret = secretsmanager.Secret.from_secret_name_v2(self, "JiraSecret", "JIRA_CREDENTIALS")
-
-       
-      
         jira_secret.grant_read(lambda_role)
         
+        pinecone_secrets = secretsmanager.Secret.from_secret_name_v2(self, "PineconeSecrets", "PINECONE_DB_SECRETS")
+        pinecone_secrets.grant_read(lambda_role)
+
         request_layer = _lambda.LayerVersion(
             self, "RequestsLayer",
-            code=_lambda.Code.from_asset("request-layer/request-layer.zip"),  
+            code=_lambda.Code.from_asset("layer/request-layer.zip"),  
             compatible_runtimes=[_lambda.Runtime.PYTHON_3_9],
             description="Layer with requests module"
         )
 
         dotenv_layer = _lambda.LayerVersion(
             self, "DotEnvLayer",
-            code=_lambda.Code.from_asset("dotenv-layer/dotenv-layer.zip"),  
+            code=_lambda.Code.from_asset("layer/dotenv-layer.zip"),  
             compatible_runtimes=[_lambda.Runtime.PYTHON_3_9],
             description="Layer with dotenv module"
         )
+        pinecone_layer = _lambda.LayerVersion(
+            self, "PineconeLayer",
+            code=_lambda.Code.from_asset("layer/pinecone.zip"),
+            compatible_runtimes=[_lambda.Runtime.PYTHON_3_9],
+        )
  
 
-        def create_lambda_function(id, name, handler, include_dir, method, layers):
+        def create_lambda_function(id, handler, include_dir, method, layers, environment):
             function = _lambda.Function(
                 self, id,
-                function_name=name,
                 runtime=_lambda.Runtime.PYTHON_3_9,
                 layers=layers,
                 handler=handler,
                 code=_lambda.Code.from_asset(include_dir),
                 memory_size=512,
                 timeout=Duration.seconds(60),     
-                environment={
-                    "JIRA_SECRET_ARN": jira_secret.secret_arn,
-                    "JIRA_URL" :'https://jiralevi9internship2025.atlassian.net/rest/api/2/search?jql=project=SCRUM',
-                    "JIRA_EMAIL" :'grubor.masa@gmail.com',
-                    "JIRA_URL_COMMENTS": 'https://jiralevi9internship2025.atlassian.net/rest/api/2/search?jql=project=SCRUM&maxResults=100&fields=comment'
-                },
+                environment = environment,
                 role=lambda_role
             )
             return function
 
         get_tickets_lambda_function=create_lambda_function(
-            "getTickets",  
-            "getTicketsFunction",  
-            "getTickets.lambda_handler",  
-            "backend/getTickets",  
+            "SaveIssues",  
+            "saveIssues.handler",  
+            "lambda",  
             "GET",  
-            [request_layer,dotenv_layer]
+            [request_layer, dotenv_layer, pinecone_layer],
+            {
+                "JIRA_SECRET_ARN": jira_secret.secret_arn,
+                "JIRA_URL" :'https://jiralevi9internship2025.atlassian.net/rest/api/2/search?jql=project=SCRUM',
+                "JIRA_EMAIL" :'grubor.masa@gmail.com',
+                "JIRA_URL_COMMENTS": 'https://jiralevi9internship2025.atlassian.net/rest/api/2/search?jql=project=SCRUM&maxResults=100&fields=comment',
+                "PINECONE_SECRET_ARN": pinecone_secrets.secret_arn,
+            }
         )
 
         get_tickets_integration = apigateway.LambdaIntegration(get_tickets_lambda_function)  
@@ -101,9 +100,6 @@ class BackendStack(Stack):
                    bucket_name="my-unique-bucket-name12312312311",
                    removal_policy=RemovalPolicy.DESTROY)
 
-
-        pinecone_secrets = secretsmanager.Secret.from_secret_name_v2(self, "PineconeSecrets", "PINECONE_DB_SECRETS")
-
         lambda_role = iam.Role(self, "LambdaBedrockRole",
                                assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
                                managed_policies=[
@@ -112,22 +108,14 @@ class BackendStack(Stack):
                                    iam.ManagedPolicy.from_aws_managed_policy_name("AmazonBedrockFullAccess")
                                ])
         
-        pinecone_secrets.grant_read(lambda_role)
-        
-        pinecone_layer = _lambda.LayerVersion(self, "PineconeLayer",
-                                             code=_lambda.Code.from_asset("layer/pinecone.zip"),
-                                             compatible_runtimes=[_lambda.Runtime.PYTHON_3_9],
-                                             )
-        
-        setPineconeDB = _lambda.Function(self, "SetPineconeDB",
-                                           runtime=_lambda.Runtime.PYTHON_3_9,
-                                           handler="setPineconeDB.handler",
-                                           code=_lambda.Code.from_asset("lambda"),
-                                           role=lambda_role,
-                                           timeout= Duration.minutes(5),
-                                           environment={
-                                               "PINECONE_SECRET_ARN": pinecone_secrets.secret_arn,
-                                               "PINECONE_INDEX_NAME": "index-name",
-                                           },
-                                           layers=[pinecone_layer])
+        setPineconeDB = create_lambda_function(
+            "SetPineconeDB",
+            "setPineconeDB.handler",
+            "lambda",
+            "PUT",
+            [pinecone_layer],
+            {
+                "PINECONE_SECRET_ARN": pinecone_secrets.secret_arn,
+            }
+        )
         
