89da862e8129576c3b2c2ae5e0149a1eb82e984d
Nikola <n.maric1912@gmail.com>
2025-03-28 11:02:44 +0100

    feat: SCRUM 21 add pinecone secrets_anager in backend_stack

--- backend/backend/backend_stack.py
+++ backend/backend/backend_stack.py
@@ -1,8 +1,15 @@
 from aws_cdk import (
+    aws_secretsmanager as secretsmanager,
+    aws_bedrock as bedrock,
+    RemovalPolicy, 
+    Duration,
     aws_apigateway as apigateway,
     Stack,
-
+    aws_s3 as s3,
+    aws_lambda as _lambda,
+    aws_iam as iam
 )
+
 from constructs import Construct
 
 class BackendStack(Stack):
@@ -26,3 +33,39 @@ class BackendStack(Stack):
         #     self, "BackendQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+
+
+        bucket = s3.Bucket(self, "my-bucket",
+                   bucket_name="my-unique-bucket-name12312312311",
+                   removal_policy=RemovalPolicy.DESTROY)
+
+
+        pinecone_secrets = secretsmanager.Secret.from_secret_name_v2(self, "PineconeSecrets", "PINECONE_DB_SECRETS")
+
+        lambda_role = iam.Role(self, "LambdaBedrockRole",
+                               assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+                               managed_policies=[
+                                   iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole"),
+                                   iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3ReadOnlyAccess"),
+                                   iam.ManagedPolicy.from_aws_managed_policy_name("AmazonBedrockFullAccess")
+                               ])
+        
+        pinecone_secrets.grant_read(lambda_role)
+        
+        pinecone_layer = _lambda.LayerVersion(self, "PineconeLayer",
+                                             code=_lambda.Code.from_asset("layer/pinecone.zip"),
+                                             compatible_runtimes=[_lambda.Runtime.PYTHON_3_9],
+                                             )
+        
+        setPineconeDB = _lambda.Function(self, "SetPineconeDB",
+                                           runtime=_lambda.Runtime.PYTHON_3_9,
+                                           handler="setPineconeDB.handler",
+                                           code=_lambda.Code.from_asset("lambda"),
+                                           role=lambda_role,
+                                           timeout= Duration.minutes(5),
+                                           environment={
+                                               "PINECONE_SECRET_ARN": pinecone_secrets.secret_arn,
+                                               "PINECONE_INDEX_NAME": "index-name",
+                                           },
+                                           layers=[pinecone_layer])
+        
