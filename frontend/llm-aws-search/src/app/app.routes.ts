879c8acb16b5fddfb6505691c5312db636976d29
vesnazubac <105123324+vesnazubac@users.noreply.github.com>
2025-04-02 10:55:55 +0200

    Implemented user registration from frontend component -SCRUM 107

--- frontend/llm-aws-search/src/app/app.routes.ts
+++ frontend/llm-aws-search/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ChatbotComponent } from './chatbot/chatbot.component';
 import { AppComponent } from './app.component';
+import { AccountConfirmationComponent } from './account-confirmation/account-confirmation.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 export const routes: Routes = [
   // Redirect the root route ("/") to the chat route ("/chat")
@@ -12,5 +14,8 @@ export const routes: Routes = [
   // You can add additional routes here if you want to handle other pages
   
   // Example for a 404 route
-  { path: '**', redirectTo: '/chat' },  // Redirect unknown routes to /chat
+  //{ path: '**', redirectTo: '/chat' },  // Redirect unknown routes to /chat
+
+  {component:AccountConfirmationComponent,path:"verifyAccount/:username"},
+  {path:'register',component:RegistrationComponent}
 ];
