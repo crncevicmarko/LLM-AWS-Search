3eb06bdc478fa7b9cc41174c4b50b97358314433
crncevicmarko <101600846+crncevicmarko@users.noreply.github.com>
2025-04-09 09:55:50 +0200

    refacotor: auth configuration

--- frontend/llm-aws-search/src/enviroments/enviroment.ts
+++ frontend/llm-aws-search/src/enviroments/enviroment.ts
@@ -3,7 +3,7 @@ export const environment = {
     apiUrl: 'https://8gq5dgs5tl.execute-api.eu-west-1.amazonaws.com/prod',
     dinamoDBUrlGetChatHistory: 'https://oud0d0fhr1.execute-api.eu-west-1.amazonaws.com/prod/get-messages',
     dinamoDBUrlPostChatHistory: 'https://oud0d0fhr1.execute-api.eu-west-1.amazonaws.com/prod/save-message',
-    userPoolId:"eu-west-1_GbRwE04nF",
-    userPoolClientId:"6fe9ffjtu8hm1ct54d90o5482q"
+    userPoolId:"eu-west-1_oZ2KyuGfa",
+    userPoolClientId:"14nim0hkscne1u1bg2njckv9km"
   };
   
