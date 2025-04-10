115dbbc03bc686cd06d157289ec6138cd82ca3f0
crncevicmarko <101600846+crncevicmarko@users.noreply.github.com>
2025-04-10 22:59:18 +0200

    fix: SCRUM-64 env.ts variable changes

--- frontend/llm-aws-search/src/env/env.ts
+++ frontend/llm-aws-search/src/env/env.ts
@@ -1,10 +1,5 @@
 export const environment = {
-<<<<<<< HEAD
     userPoolId:"eu-west-1_n9gwGDa9m",
     userPoolClientId:"aeiv6iipl7grttkskkkquc6gr"
-=======
-    userPoolId:"eu-west-1_boxUTqFxU",
-    userPoolClientId:"1ir3ru2fmdm4j55icaq8poei68",
->>>>>>> develop
   }
   
