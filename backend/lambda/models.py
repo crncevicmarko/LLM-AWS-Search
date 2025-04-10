c03f4f886f9069129b73814935195efff886646f
Nikola <n.maric1912@gmail.com>
2025-04-10 14:34:57 +0200

    faet: SCRUM-169 store more metadata like assignee, status, time_created and time_updated in Pinecone

--- backend/lambda/models.py
+++ backend/lambda/models.py
@@ -1,13 +1,17 @@
 from typing import List, Dict
 
 class IssueVector:
-    def __init__(self, text_id: str, embedding: List[float],id, text: str, creator ,url: str):
+    def __init__(self, text_id: str, embedding: List[float],id, text: str, creator, assignee, time_created, time_updated, status, url: str):
         self.id = text_id
         self.values = embedding
         self.metadata = {
             "id": id,
             "text": text,
             "creator": creator,
+            "assignee": assignee,
+            "time-created": time_created,
+            "time-updated": time_updated,
+            "status": status,
             "ticket-url": url
         }
     def set_embedding(self, embedding: List[float]):
